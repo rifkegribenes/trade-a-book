6efef723807d495b4b37ca3c828e8e85a26891eb
rifkegribenes <rifkegribenes@gmail.com>
2018-08-31 19:52:55 -0700

    add static controller back in...

--- router.js
+++ router.js
@@ -153,6 +153,6 @@ module.exports = function (app) {
 
   // Catch client-side routes that don't exist on the back-end.
   // Redirects to /#/redirect={route}/{optional_id}
-  // app.get('/:client_route/:id?', StaticController.redirectHash);
+  app.get('/:client_route/:id?', StaticController.redirectHash);
 
 };
